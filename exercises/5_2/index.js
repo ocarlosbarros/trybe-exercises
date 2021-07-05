b0906ca10cd26f82ffa5b230a74b8e8af11e66b2
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-05 18:32:15 -0300

    Adiciona imagem na div left-content

--- exercises/5_2/index.js
+++ exercises/5_2/index.js
@@ -1,11 +1,12 @@
 const header = document.createElement('h1');
+const body = document.querySelector('body');
+
 const divMain = document.createElement('div');
 const divCenter = document.createElement('div');
 const paragraph = document.createElement('p');
 const divLeftContent = document.createElement('div');
 const divRightContent = document.createElement('div');
-
-const body = document.querySelector('body');
+const image = document.createElement('img');
 
 header.innerHTML = 'Exercício 5.2 - Javascript DOM';
 divMain.className = 'main-content';
@@ -13,10 +14,13 @@ divCenter.className = 'center-content';
 paragraph.innerHTML = 'Game Of Thrones';
 divLeftContent.className = 'left-content';
 divRightContent.className = 'right-content';
+image.setAttribute('src', 'https://picsum.photos/200');
+image.className = 'small-image';
 
 body.appendChild(header);
 body.appendChild(divMain);
 divMain.appendChild(divCenter);
 divMain.appendChild(divLeftContent);
+divLeftContent.appendChild(image);
 divMain.appendChild(divRightContent);
 divCenter.appendChild(paragraph);
