e8eb81349131726a44010031c8a8b43b009b14a3
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-05 18:22:22 -0300

    Adiciona divMain e divCenter no documento

--- exercises/5_2/index.js
+++ exercises/5_2/index.js
@@ -1,6 +1,14 @@
 const header = document.createElement('h1');
+const divMain = document.createElement('div');
+const divCenter = document.createElement('div');
+
 header.innerHTML = 'Exercício 5.2 - Javascript DOM';
+
+divMain.className = 'main-content';
+divCenter.className = 'center-content';
 
 const body = document.querySelector('body');
 
 body.appendChild(header);
+body.appendChild(divMain);
+divMain.appendChild(divCenter);
