bad88a1a18edb56f6ad0682847e0c605a2aad3ba
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-05 18:27:51 -0300

    Adiciona Div com conteudo a direita

--- exercises/5_2/index.js
+++ exercises/5_2/index.js
@@ -2,15 +2,21 @@ const header = document.createElement('h1');
 const divMain = document.createElement('div');
 const divCenter = document.createElement('div');
 const paragraph = document.createElement('p');
+const divLeftContent = document.createElement('div');
+const divRightContent = document.createElement('div');
+
+const body = document.querySelector('body');
 
 header.innerHTML = 'Exercício 5.2 - Javascript DOM';
 divMain.className = 'main-content';
 divCenter.className = 'center-content';
 paragraph.innerHTML = 'Game Of Thrones';
-
-const body = document.querySelector('body');
+divLeftContent.className = 'left-content';
+divRightContent.className = 'right-content';
 
 body.appendChild(header);
 body.appendChild(divMain);
 divMain.appendChild(divCenter);
+divMain.appendChild(divLeftContent);
+divMain.appendChild(divRightContent);
 divCenter.appendChild(paragraph);
