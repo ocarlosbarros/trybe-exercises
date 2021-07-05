636eed1b03e515cd7cbee77caeed64f0af375aca
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-05 18:25:08 -0300

    Adiciona paragraph dentro da div

--- exercises/5_2/index.js
+++ exercises/5_2/index.js
@@ -1,14 +1,16 @@
 const header = document.createElement('h1');
 const divMain = document.createElement('div');
 const divCenter = document.createElement('div');
+const paragraph = document.createElement('p');
 
 header.innerHTML = 'Exercício 5.2 - Javascript DOM';
-
 divMain.className = 'main-content';
 divCenter.className = 'center-content';
+paragraph.innerHTML = 'Game Of Thrones';
 
 const body = document.querySelector('body');
 
 body.appendChild(header);
 body.appendChild(divMain);
 divMain.appendChild(divCenter);
+divCenter.appendChild(paragraph);
