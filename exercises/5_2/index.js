d1c8585dffcf1316f58866ec401b58f30e29599b
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-05 18:39:33 -0300

    Adiciona lista nao ordenada

--- exercises/5_2/index.js
+++ exercises/5_2/index.js
@@ -7,6 +7,7 @@ const paragraph = document.createElement('p');
 const divLeftContent = document.createElement('div');
 const divRightContent = document.createElement('div');
 const image = document.createElement('img');
+const unorderedList = document.createElement('ul');
 
 header.innerHTML = 'Exercício 5.2 - Javascript DOM';
 divMain.className = 'main-content';
@@ -17,10 +18,20 @@ divRightContent.className = 'right-content';
 image.setAttribute('src', 'https://picsum.photos/200');
 image.className = 'small-image';
 
+const listItems = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez'];
+for (let index = 0; index < listItems.length; index += 1){
+  
+  let item = document.createElement('li');
+  item.innerText = listItems[index];
+
+  unorderedList.appendChild(item);
+}
+
 body.appendChild(header);
 body.appendChild(divMain);
 divMain.appendChild(divCenter);
 divMain.appendChild(divLeftContent);
 divLeftContent.appendChild(image);
 divMain.appendChild(divRightContent);
+divRightContent.appendChild(unorderedList);
 divCenter.appendChild(paragraph);
