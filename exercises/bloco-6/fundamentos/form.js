d016b95bf23593162ca7db220eb0b2cb60683719
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-14 18:42:01 -0300

    Adicona html + css + js files e cria campos para Dados pessoais incluindo funçao para criar option dentro do select dinamicamente

---
+++ exercises/bloco-6/fundamentos/form.js
@@ -0,0 +1,14 @@
+
+function FillSelectField(){
+  const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
+  const selectField = document.querySelector('#field-state');
+  
+  for (let index = 0; index < states.length; index += 1) {
+    const stateOption = document.createElement('option');
+    stateOption.value = states[index];
+    stateOption.innerText = states[index];
+    selectField.appendChild(stateOption);
+  }
+}
+
+FillSelectField();
